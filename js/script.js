822a5304e3e4beb33006dc348a709b78a655199d
Lorenzo Franco <lorfran14@hotmail.com>
2022-02-18 11:44:37 -0500

    fixed the functionality of the email error, and added the Readme file

--- js/script.js
+++ js/script.js
@@ -1,16 +1,20 @@
+// Select all the items that I'm going to use
 const form = document.querySelector('form');
 const firstName = document.querySelector('#first-name');
 const lastName = document.querySelector('#last-name');
 const email = document.querySelector('#email');
 const password = document.querySelector('#password');
-// stop the default of submit in the form
 
+
+
+
+// this will validate if the format of an email is correct
+const validEmailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+
+// stop the form from submitting if any of the inputs is empty
 console.log(firstName.value.length);
 form.addEventListener('submit', e => {
-    // const error = () => {
-    //     firstName.closest('.input').classList.add('error');
-    //     e.preventDefault();
-    // }
     if (firstName.value.length === 0) {
         firstName.closest('.input').classList.add('error');
         e.preventDefault();
@@ -31,9 +35,18 @@ form.addEventListener('submit', e => {
     }
     if (email.value.length === 0) {
         email.closest('.input').classList.add('error');
+        email.closest('.input').classList.remove('wrong');
         e.preventDefault();
+    } else if (!email.value.match(validEmailFormat)) {
+        console.log('match')
+        email.closest('.input').classList.remove('error');
+        email.closest('.input').classList.add('wrong');
+        e.preventDefault();
+
     } else {
         email.closest('.input').classList.remove('error');
+        email.closest('.input').classList.remove('wrong');
+
     }
 
 })
