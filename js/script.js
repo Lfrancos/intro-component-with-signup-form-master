49de7980c319c1e8531679c48d12fc86d633cd01
Lorenzo Franco <lorfran14@hotmail.com>
2022-02-25 19:24:19 -0500

    trying to make to code more "efficient". Making it shorter

--- js/script.js
+++ js/script.js
@@ -13,40 +13,58 @@ const validEmailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
 
 // stop the form from submitting if any of the inputs is empty
-console.log(firstName.value.length);
 form.addEventListener('submit', e => {
-    if (firstName.value.length === 0) {
-        firstName.closest('.input').classList.add('error');
-        e.preventDefault();
-    } else {
-        firstName.closest('.input').classList.remove('error');
-    }
-    if (lastName.value.length === 0) {
-        lastName.closest('.input').classList.add('error');
-        e.preventDefault();
-    } else {
-        lastName.closest('.input').classList.remove('error');
-    }
-    if (password.value.length === 0) {
-        password.closest('.input').classList.add('error');
-        e.preventDefault();
-    } else {
-        password.closest('.input').classList.remove('error');
-    }
-    if (email.value.length === 0) {
-        email.closest('.input').classList.add('error');
-        email.closest('.input').classList.remove('wrong');
-        e.preventDefault();
-    } else if (!email.value.match(validEmailFormat)) {
-        console.log('match')
-        email.closest('.input').classList.remove('error');
-        email.closest('.input').classList.add('wrong');
-        e.preventDefault();
+    const formInputs = [firstName, lastName, password, email];
+    formInputs.forEach(input => {
+        if (input.value.length === 0) {
+            console.log(input);
+            input.closest('.input').classList.add('error');
+            e.preventDefault();
 
-    } else {
-        email.closest('.input').classList.remove('error');
-        email.closest('.input').classList.remove('wrong');
+        } else {
+            input.closest('.input').classList.remove('error');
+        }
+    })
 
-    }
+
+
+
+
+    // this was something that worked but I need to create a way to make them work with less code. Taking advantage of the programing language.
+
+
+    // if (firstName.value.length === 0) {
+    //     firstName.closest('.input').classList.add('error');
+    //     e.preventDefault();
+    // } else {
+    //     firstName.closest('.input').classList.remove('error');
+    // }
+    // if (lastName.value.length === 0) {
+    //     lastName.closest('.input').classList.add('error');
+    //     e.preventDefault();
+    // } else {
+    //     lastName.closest('.input').classList.remove('error');
+    // }
+    // if (password.value.length === 0) {
+    //     password.closest('.input').classList.add('error');
+    //     e.preventDefault();
+    // } else {
+    //     password.closest('.input').classList.remove('error');
+    // }
+    // if (email.value.length === 0) {
+    //     email.closest('.input').classList.add('error');
+    //     email.closest('.input').classList.remove('wrong');
+    //     e.preventDefault();
+    // } else if (!email.value.match(validEmailFormat)) {
+    //     console.log('match')
+    //     email.closest('.input').classList.remove('error');
+    //     email.closest('.input').classList.add('wrong');
+    //     e.preventDefault();
+
+    // } else {
+    //     email.closest('.input').classList.remove('error');
+    //     email.closest('.input').classList.remove('wrong');
+
+    // }
 
 })
